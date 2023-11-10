33590c45aa651e1da50136a9a6f0fa9a8d4e93ba
hyunjin <guswlsdl0121@gmail.com>
2023-11-10 22:45:39 +0900

    test: DateUtilsTest 작성

---
+++ src/test/java/christmas/util/DateUtilsTest.java
@@ -0,0 +1,29 @@
+package christmas.util;
+
+import christmas.config.EventDate;
+import java.time.DayOfWeek;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DateUtilsTest {
+    @Test
+    @DisplayName("주어진 날짜의 요일을 올바르게 반환한다.")
+    void testGetDayOfWeek() {
+        //given
+        DayOfWeek expectedDayOfWeek = DayOfWeek.FRIDAY;
+
+        //when
+        DayOfWeek actualDayOfWeek = DateUtils.getDayOfWeek(1);
+
+        //then
+        Assertions.assertEquals(expectedDayOfWeek, actualDayOfWeek);
+    }
+
+    @Test
+    @DisplayName("크리스마스 날짜 여부를 올바르게 확인한다.")
+    void testIsChristmasDay() {
+        Assertions.assertTrue(DateUtils.isChristmasDay(EventDate.CHRISTMAS_DATE.getValue())); // 크리스마스 날짜
+        Assertions.assertFalse(DateUtils.isChristmasDay(24)); // 크리스마스 이브
+    }
+}
