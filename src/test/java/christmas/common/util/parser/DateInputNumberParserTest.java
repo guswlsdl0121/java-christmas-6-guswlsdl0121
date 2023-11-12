90f09e12722045086f7eebf10bb23e660ab83be5
hyunjin <guswlsdl0121@gmail.com>
2023-11-13 01:17:41 +0900

    test: 식당 방문 날짜 입력 구현 Test 진행

---
+++ src/test/java/christmas/common/util/parser/DateInputNumberParserTest.java
@@ -0,0 +1,40 @@
+package christmas.common.util.parser;
+
+import java.time.LocalDate;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class DateInputNumberParserTest {
+
+    private static Stream<Arguments> provideInputsForParsing() {
+        return Stream.of(
+                // 정상적인 입력
+                Arguments.of("3", LocalDate.of(2023, 12, 3), false),
+                Arguments.of("15", LocalDate.of(2023, 12, 15), false),
+                // 비정상적인 입력
+                Arguments.of("-1", null, true),
+                Arguments.of("2147483648", null, true),
+                Arguments.of("3.5", null, true),
+                Arguments.of("@3", null, true),
+                Arguments.of("일", null, true),
+                // 비정상적인 날짜
+                Arguments.of("32", null, true),
+                Arguments.of("0", null, true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInputsForParsing")
+    void testDateInputNumberParser(String input, LocalDate expectedDate, boolean expectException) {
+        DateInputNumberParser parser = new DateInputNumberParser();
+        if (expectException) {
+            Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parse(input));
+        } else {
+            LocalDate parsedDate = parser.parse(input);
+            Assertions.assertEquals(expectedDate, parsedDate);
+        }
+    }
+}
